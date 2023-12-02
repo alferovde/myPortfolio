2075fcc61f2af77c9cd3bfbb685f80919e78a7c6
Dmitriy <66624150+alferovde@users.noreply.github.com>
2023-12-02 12:06:10 +0300

    Revert "finish"
    This reverts commit d9a028f282862a3b96b5f86b1c9d66301ea8616a.

--- src/modules/myjs.js
+++ src/modules/myjs.js
@@ -1,67 +1,17 @@
-// export const calc = (a, b) => a + b;
+export const calc = (a, b) => a + b;
 $(".owl-carousel").owlCarousel({
   loop: true,
   margin: 10,
   nav: true,
-  navText: ["&#10094;", "&#10095;"],
-  itemElement: "news-item",
   responsive: {
     0: {
       items: 1,
     },
-    400: {
-      items: 1,
-    },
-    500: {
-      items: 2,
-    },
     600: {
-      items: 2,
-    },
-    1000: {
       items: 3,
     },
+    1000: {
+      items: 5,
+    },
   },
-});
-
-let $window = $(window),
-  previousScrollTop = 0,
-  scrollLock = false;
-
-$window.scroll(function (event) {
-  if (scrollLock) {
-    $window.scrollTop(previousScrollTop);
-  }
-
-  previousScrollTop = $window.scrollTop();
-});
-
-let mobileMenu__wrapper = document.querySelector(".mobile-menu__wrapper");
-let hamburger = document.querySelector(".hamburger");
-let idToggle = document.querySelector("#toggle");
-
-hamburger.addEventListener("click", () => {
-  console.log(idToggle.checked);
-  if (idToggle.checked) {
-    mobileMenu__wrapper.classList.add("animate__bounceOutLeft");
-    mobileMenu__wrapper.classList.remove("animate__bounceInLeft");
-    setTimeout(() => {
-      mobileMenu__wrapper.classList.remove("animate__bounceOutLeft");
-      mobileMenu__wrapper.classList.add("hidden");
-      scrollLock = false;
-    }, 500);
-  } else {
-    mobileMenu__wrapper.classList.remove("hidden");
-    mobileMenu__wrapper.classList.add("animate__bounceInLeft");
-
-    scrollLock = true;
-  }
-});
-
-window.addEventListener("resize", () => {
-  if (window.screen.width > 1000) {
-    mobileMenu__wrapper.classList.add("hidden");
-    idToggle.checked = false;
-    scrollLock = false;
-  }
 });
